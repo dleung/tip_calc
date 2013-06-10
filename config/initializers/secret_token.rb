67ea0f62cb628e77db0bcc6dd76f8b92f7bc701c
David Leung <davleun@gmail.com>
2013-06-09 20:17:07 -0700

    Added angular resources

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AngularRailsDemo::Application.config.secret_token = '0c1771aa1df1a76bb971a72363e3c934d7961894ec0c1904b2925b43ab75c946089e14e5d3bf02b21861cd1c4a041e55c7d3346668b1eb086295dab6d4841d12'
